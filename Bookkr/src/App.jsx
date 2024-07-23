dc5329e98750a6406dca8dac99f935145af9d49f
Aditya <adityahariharan16@gmail.com>
2024-07-23 21:24:13 +0530

    Components added

--- Bookkr/src/App.jsx
+++ Bookkr/src/App.jsx
@@ -3,14 +3,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header'
-import Navbar from './components/Navbar'
-import Carousel from './components/Carousel'
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <Carousel/>
+    <Header/>
     </>
   )
 }
